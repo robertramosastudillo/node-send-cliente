a5d80d0105ecc4a4a682638318079f54e2fa9e04
robertramosastudillo <robertramosastudillo@gmail.com>
2020-11-03 20:42:39 -0500

    Se trabajo en el link de descaga del archivo

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,38 @@
 import React, { useContext, useEffect } from "react";
 import Link from "next/link";
 import authContext from "../context/auth/authContext";
+import appContext from "../context/app/appContext";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  // routing
+  const router = useRouter();
+
   // Extraer el Usuario autenticado del storage
   const AuthContext = useContext(authContext);
   const { usuario, usuarioAutenticado, cerrarSesion } = AuthContext;
+
+  // Context de la apicacion
+  const AppContext = useContext(appContext);
+  const { limpiarState } = AppContext;
 
   useEffect(() => {
     usuarioAutenticado();
   }, []);
 
+  const redireccionar = () => {
+    router.push("/");
+
+    limpiarState();
+  };
+
   return (
     <header className="py-8 flex flex-col md:flex-row items-center justify-between">
-      <Link href="/">
-        <img className="w-64 mb-8 md:mb-0" src="/logo.svg" />
-      </Link>
+      <img
+        onClick={() => redireccionar()}
+        className="w-64 mb-8 md:mb-0 cursor-pointer"
+        src="/logo.svg"
+      />
       <div>
         {usuario ? (
           <div className="flex items-center">
--- pages/enlaces/[enlace].js
+++ pages/enlaces/[enlace].js
@@ -1,7 +1,7 @@
 import Layout from "../../components/Layout";
 import clienteAxios from "../../config/axios";
 
-export async function getStaticProps({ params }) {
+export async function getServerSideProps({ params }) {
   const { enlace } = params;
   const resultado = await clienteAxios.get(`/api/enlaces/${enlace}`);
 
@@ -12,7 +12,7 @@ export async function getStaticProps({ params }) {
   };
 }
 
-export async function getStaticPaths() {
+export async function getServerSidePaths() {
   const enlaces = await clienteAxios.get("/api/enlaces");
 
   return {
@@ -27,7 +27,18 @@ export default ({ enlace }) => {
   console.log(enlace);
   return (
     <Layout>
-      <h1>Desde [enlace].js</h1>
+      <h1 className="text-4xl text-center text-gray-700">
+        Descarga tu archivo
+      </h1>
+      <div className="flex items-center justify-center mt-10">
+        <a
+          href={`${process.env.backendUrl}/api/archivos/${enlace.archivo}`}
+          className="bg-red-500 text-center px-10 py-3 rounded uppercase font-bold text-white cursor-pointer"
+          download
+        >
+          Aquí
+        </a>
+      </div>
     </Layout>
   );
 };
